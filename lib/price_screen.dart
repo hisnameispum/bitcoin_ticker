b25fe04768f028e2001daa8afad2941108f65060
hisnameispum <sirasitxp@gmail.com>
2020-08-23 03:48:18 -0700

    Added HTTP package and URL

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:bitcoin_ticker/coin_data.dart' as coin_data;
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
 import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
@@ -62,6 +64,8 @@ class _PriceScreenState extends State<PriceScreen> {
   @override
   Widget build(BuildContext context) {
     const apiKey = '05EE6727-F13B-4F59-B60A-6B4CF8AE7E69';
+    const url= 'curl https://rest.coinapi.io/v1/exchangerate/BTC/USD \ --request GET --header "X-CoinAPI-Key: ${apiKey} ';
+
     currencyList = returnText();
     return Scaffold(
       appBar: AppBar(
