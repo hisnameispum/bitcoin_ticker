d68cfb14d7966cf3b94878f503172a9afb9eeaa5
hisnameispum <sirasitxp@gmail.com>
2020-08-25 03:10:38 -0700

    Finalizing

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -35,30 +35,25 @@ const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
 const apiKey = '05EE6727-F13B-4F59-B60A-6B4CF8AE7E69';
 
 class CoinData {
-  //3. Create the Asynchronous method getCoinData() that returns a Future (the price data).
   Future getCoinData(String selectedCurrency) async {
-    //4. Create a url combining the coinAPIURL with the currencies we're interested, BTC to USD.
+    //4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+    //5: Return a Map of the results instead of a single value.
     Map<String, String> cryptoPrices = {};
     for (String crypto in cryptoList) {
       //Update the URL to use the crypto symbol from the cryptoList
       String requestURL =
           '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
       http.Response response = await http.get(requestURL);
-
-      //6. Check that the request was successful.
       if (response.statusCode == 200) {
-        //7. Use the 'dart:convert' package to decode the JSON data that comes back from coinapi.io.
         var decodedData = jsonDecode(response.body);
-        //8. Get the last price of bitcoin with the key 'last'.
-        var lastPrice = decodedData['rate'];
-        //9. Output the lastPrice from the method.
-        return lastPrice.toStringAsFixed(0);
+        double lastPrice = decodedData['rate'];
+        //Create a new key value pair, with the key being the crypto symbol and the value being the lastPrice of that crypto currency.
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
       } else {
-        //10. Handle any errors that occur during the request.
         print(response.statusCode);
-        //Optional: throw an error if our request fails.
         throw 'Problem with the get request';
       }
     }
+    return cryptoPrices;
   }
 }
